57f2142df9333d4ea6a1a3f4def3a070d860f22e
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-19 03:00:47 +0200

    custom text form field in translation view

---
+++ lib/features/translation/widgets/custom_input.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFieldForTranslate extends StatelessWidget {
+  const CustomTextFieldForTranslate({
+    super.key,
+    required this.transController,
+    required this.translateTextField,
+    required this.lang1,
+  });
+  final TextEditingController transController;
+  final Function translateTextField;
+  final String lang1;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: transController,
+      onChanged: (value) {
+        translateTextField(value);
+      },
+      onTap: () => translateTextField(transController.text),
+      onSubmitted: (value) => translateTextField(value),
+      autocorrect: true,
+      style: const TextStyle(
+        fontSize: 22,
+        color: Colors.white,
+        fontWeight: FontWeight.w400,
+      ),
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: lang1 == 'Arabic' ? 'ادخل النص' : 'Enter text',
+        hintTextDirection:
+            lang1 == 'Arabic' ? TextDirection.rtl : TextDirection.ltr,
+        hintStyle: const TextStyle(
+          fontSize: 30,
+          color: Colors.grey,
+          fontWeight: FontWeight.w400,
+        ),
+        contentPadding: const EdgeInsets.all(18),
+      ),
+      textDirection: lang1 == 'Arabic' ? TextDirection.rtl : TextDirection.ltr,
+    );
+  }
+}
